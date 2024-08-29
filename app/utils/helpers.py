d32f942bdfb20bec41ebf487f0185ac8d010613e
Idris Abdurrahman <abdurrahmaneedrees@gmail.com>
2024-08-29 16:02:41 +0100

    add: Add helper functions for various tasks

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,5 +1,7 @@
 import re
 from datetime import datetime
+from app.models import Message
+
 
 def generate_slug(text):
     """
@@ -9,6 +11,7 @@ def generate_slug(text):
     text = re.sub(r'[^\w\-]', '', text)
     return text.lower()
 
+
 def calculate_age(birthdate):
     """
     Calculates the age based on the provided birthdate.
@@ -16,6 +19,18 @@ def calculate_age(birthdate):
     today = datetime.today()
     return today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
 
+
 def format_response(message, status_code=200):
     return {'message': message}, status_code
 
+
+def truncate_words(text, num_words):
+    words = text.split()
+    if len(words) > num_words:
+        return ' '.join(words[:num_words]) + '...'
+    return text
+
+
+def get_unread_message_count(user_id):
+    unread_count = Message.query.filter_by(recipient_id=user_id, is_read=False).count()
+    return unread_count

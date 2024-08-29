cf6cd39c9a633f3b057f38946d3ed0b07f18785e
Idris Abdurrahman <abdurrahmaneedrees@gmail.com>
2024-08-29 16:03:41 +0100

    add: Set up Flask extensions

--- app/extensions.py
+++ app/extensions.py
@@ -1,6 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mail import Mail
+from flask_login import LoginManager
 
 
 db = SQLAlchemy()
 migrate = Migrate()
+mail = Mail()
+login_manager = LoginManager()
